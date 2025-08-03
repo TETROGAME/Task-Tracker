bf026d3c50e584f2691e4654e4f84bd9969df27f
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-08-03 23:09:25 +0300

    Add info and input loop

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
+from unittest import case
+
 from TaskTracker import *
 
 def main() -> int:
-    tasks = [Task(1, 'generic task')]
-    tracker = TaskTracker(tasks)
-    print(tracker)
-    tracker.add_task()
-    print(tracker)
-    # tracker.update_task(task_id = 1)
-    # print(tracker)
+    print('Welcome to Task Tracker, a simple way to manage your tasks using CLI!')
+    while True:
+        print('Available options:\n'
+              '1. Print existing tasks\n'
+              '2. Add a new task\n'
+              '3. Edit an existing task\n'
+              '4. Delete an existing task\n'
+              '5. Exit\n\n'
+              'To quit without saving type \'q\'\n'
+              'To save and quit type \'sq\'\n'
+              'To save type \'s\'\n'
+              'To load type \'l\'\n')
+        user_input = input('> ')
+        match user_input:
+            case '1':
+                pass
+            case '2':
+                pass
+            case '3':
+                pass
+            case '4':
+                pass
+            case '5':
+                pass
+            case 'q':
+                pass
+            case 'sq':
+                pass
+            case 's':
+                pass
+            case 'l':
+                pass
+            case _:
+                print('Invalid input. Please try again.')
+                break
     return 0
 
 
