0971a6e1009aa3827c5941dc134b0e1332e2698d
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-08-03 10:25:44 +0300

    Add TaskTracker class basic functionality + TaskError exception

--- Task Tracker.py
+++ Task Tracker.py
@@ -1,13 +1,18 @@
 from datetime import datetime
 
-status_list = ('In progress', 'Done')
+STATUS_LIST = ('In progress', 'Done')
+
+class TaskError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
 class Task:
     _id: int
     _description: str
     _status: str
     _cratedAt: datetime
     _updatedAt: datetime
-    def __init__(self, _id: int = 0, _description: str = '', _status: str = status_list[0]
+    def __init__(self, _id: int = 0, _description: str = '', _status: str = STATUS_LIST[0]
                  , _cratedAt: datetime = datetime.date(datetime.now())
                  , _updatedAt: datetime = datetime.date(datetime.now())):
         self._id = _id
@@ -19,11 +24,27 @@ class Task:
         return (f'ID: {self._id}\nDescription: {self._description}\n'
                 f'Status: {self._status}\nCreated at: {self._cratedAt}\n'
                 f'Updated at: {self._updatedAt}')
+    def is_valid(self):
+        pass
+
+
+class TaskTracker:
+    tasks: list[Task]
+    def __init__(self, tasks: list[Task] = None):
+        if tasks is None:
+            tasks = []
+        self.tasks = tasks
+    def __repr__(self):
+        return f'Stored tasks: {self.tasks}'
+
+    def add_task(self, task: Task):
+        if task.is_valid(): self.tasks.append(task)
+        else: raise TaskError('Task is not valid')
+        return self
+
 
 def main() -> int:
     return 0
-
-
 
 
 if __name__ == '__main__':
