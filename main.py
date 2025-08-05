82c3f55954ecbe52a69a8cfa70fa0e2f4f273db1
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-08-05 20:44:44 +0300

    Refactored print_tasks method

--- main.py
+++ main.py
@@ -6,22 +6,31 @@ def print_tasks(tracker: TaskTracker) -> None:
           '\'1\' Print all existing tasks\n'
           '\'2\' Print all done tasks\n'
           '\'3\' Print all in progress tasks\n'
-          '\'4\' Print all not done tasks\n')
-        match input(':'):
+          '\'4\' Print all not done tasks')
+        match input('> '):
             case '1':
                 print(f'Tasks list:\n{tracker}\n')
             case '2':
-                print('Done tasks list:')
-                for task in tracker.get_done_tasks():
-                    print(task)
+                if tracker.get_done_tasks():
+                    print('Done tasks list:')
+                    for task in tracker.get_done_tasks():
+                        print(task)
+                else:
+                    print('No tasks with status \'done\'')
             case '3':
-                print('In progress tasks list:')
-                for task in tracker.get_in_progress_tasks():
-                    print(task)
+                if tracker.get_in_progress_tasks():
+                    print('In progress tasks list:')
+                    for task in tracker.get_in_progress_tasks():
+                        print(task)
+                else:
+                    print('No tasks with status \'in progress\'')
             case '4':
-                print('Not done tasks:')
-                for task in tracker.get_not_done_tasks():
-                    print(task)
+                if tracker.get_not_done_tasks():
+                    print('Not done tasks:')
+                    for task in tracker.get_not_done_tasks():
+                        print(task)
+                else:
+                    print('No tasks with status not \'done\'')
             case _:
                 print('Invalid input')
     else:
@@ -51,7 +60,7 @@ def main() -> int:
     print('Welcome to Task Tracker, a simple way to manage your tasks using CLI!')
     while True:
         print('Available options:\n'
-              '\'1\' Print existing tasks\n'
+              '\'1\' Print tasks\n'
               '\'2\' Add a new task\n'
               '\'3\' Edit an existing task\n'
               '\'4\' Delete an existing task\n'
