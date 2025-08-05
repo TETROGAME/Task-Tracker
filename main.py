a8d37deedb95215815972624ca0d1534fc60a7a3
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-08-05 20:16:32 +0300

    improved user input logic

--- main.py
+++ main.py
@@ -1,4 +1,51 @@
 from TaskTracker import *
+
+def print_tasks(tracker: TaskTracker) -> None:
+    if tracker.get_tasks():
+        print('Select print options:\n'
+          '\'1\' Print all existing tasks\n'
+          '\'2\' Print all done tasks\n'
+          '\'3\' Print all in progress tasks\n'
+          '\'4\' Print all not done tasks\n')
+        match input(':'):
+            case '1':
+                print(f'Tasks list:\n{tracker}\n')
+            case '2':
+                print('Done tasks list:')
+                for task in tracker.get_done_tasks():
+                    print(task)
+            case '3':
+                print('In progress tasks list:')
+                for task in tracker.get_in_progress_tasks():
+                    print(task)
+            case '4':
+                print('Not done tasks:')
+                for task in tracker.get_not_done_tasks():
+                    print(task)
+            case _:
+                print('Invalid input')
+    else:
+        print('No tasks saved\n')
+
+def validate_task_id(func):
+    def wrapper(tracker):
+        user_input = input('Enter task ID: ')
+        try:
+            task_id = int(user_input)
+            return func(tracker, task_id)
+        except ValueError as error:
+            print(f"Invalid task ID: {error}")
+            return False
+    return wrapper
+
+@validate_task_id
+def update_task_option(tracker: TaskTracker, task_id: int):
+    tracker.update_task(task_id)
+
+@validate_task_id
+def delete_task_option(tracker: TaskTracker, task_id: int):
+    tracker.delete_task(task_id)
+
 def main() -> int:
     tracker = TaskTracker()
     print('Welcome to Task Tracker, a simple way to manage your tasks using CLI!')
@@ -16,24 +63,13 @@ def main() -> int:
         user_input = input('> ')
         match user_input:
             case '1':
-                if tracker.get_tasks():
-                    print(f'Tasks list:\n{tracker}\n')
-                else:
-                    print('No tasks saved\n')
+                print_tasks(tracker)
             case '2':
                 tracker.add_task()
             case '3':
-                user_input = input('Enter task ID: ')
-                try:
-                    tracker.update_task(int(user_input))
-                except ValueError as error:
-                    print(error)
+                update_task_option(tracker)
             case '4':
-                user_input = input('Enter new task ID: ')
-                try:
-                    tracker.delete_task(int(user_input))
-                except ValueError as error:
-                    print(error)
+                delete_task_option(tracker)
             case 'q':
                 print('Quitting...')
                 exit(0)
